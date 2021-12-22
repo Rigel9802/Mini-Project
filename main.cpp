79ac5c6bf9bfd934bc9426bd47b52ce8e1b52060
Lucky Alma Aficionado Rigel <luckyrigel9802@gmail.com>
2021-12-23 07:08:26 +0800

    terbaru

--- main.cpp
+++ main.cpp
@@ -13,7 +13,14 @@ struct Node
 Node *cur, *temp, *bef, *del;
 // deklarasi striker
 Node *headStriker = NULL, *tailStriker;
+// deklarasi midfielder
+Node *headMidfielder = NULL, *tailMidfielder;
+// deklarasi defender
+Node *headDefender = NULL, *tailDefender;
+// deklarasi goalkeeper
+Node *headGoalkeeper = NULL, *tailGoalkeeper;
 
+// create parent
 void createParent(Node *temp, string role)
 {
     temp = new Node;
@@ -31,35 +38,9 @@ void createParent(Node *temp, string role)
         parentTail = temp;
     }
 }
+// end functions create parent
 
-void tambahStriker(Node *temp, string NmPlayer, int NuPlayer)
-{
-    cur = parentHead;
-    while (cur != NULL)
-    {
-        if (cur->role == "striker")
-        {
-            temp = new Node;
-            temp->NmPlayer = NmPlayer;
-            temp->NuPlayer = NuPlayer;
-            if (headStriker == NULL)
-            {
-                cur->child = temp;
-                temp->child = NULL;
-                headStriker = temp;
-                tailStriker = headStriker;
-            }
-            else
-            {
-                temp->child = NULL;
-                tailStriker->child = temp;
-                tailStriker = temp;
-            }
-        }
-        cur = cur->next;
-    }
-}
-
+// start functions checkChild
 bool checkChild(Node *temp, string checkPlayer, string role)
 {
     temp = parentHead;
@@ -95,46 +76,172 @@ bool checkChild(Node *temp, string checkPlayer, string role)
     }
     return 0;
 }
+// end functions check child
+
+// striker functions
+void tambahStriker(Node *temp, string NmPlayer, int NuPlayer)
+{
+    cur = parentHead;
+    while (cur != NULL)
+    {
+        if (cur->role == "striker")
+        {
+            temp = new Node;
+            temp->NmPlayer = NmPlayer;
+            temp->NuPlayer = NuPlayer;
+            if (headStriker == NULL)
+            {
+                cur->child = temp;
+                temp->child = NULL;
+                headStriker = temp;
+                tailStriker = headStriker;
+            }
+            else
+            {
+                temp->child = NULL;
+                tailStriker->child = temp;
+                tailStriker = temp;
+            }
+        }
+        cur = cur->next;
+    }
+}
+
+// midfielder functions
+void tambahMidfielder(Node *temp, string NmPlayer, int NuPlayer)
+{
+    cur = parentHead;
+    while (cur != NULL)
+    {
+        if (cur->role == "midfielder")
+        {
+            temp = new Node;
+            temp->NmPlayer = NmPlayer;
+            temp->NuPlayer = NuPlayer;
+            if (headMidfielder == NULL)
+            {
+                cur->child = temp;
+                temp->child = NULL;
+                headMidfielder = temp;
+                tailMidfielder = headMidfielder;
+            }
+            else
+            {
+                temp->child = NULL;
+                tailMidfielder->child = temp;
+                tailMidfielder = temp;
+            }
+        }
+        cur = cur->next;
+    }
+}
+
+// defender functions
+void tambahDefender(Node *temp, string NmPlayer, int NuPlayer)
+{
+    cur = parentHead;
+    while (cur != NULL)
+    {
+        if (cur->role == "defender")
+        {
+            temp = new Node;
+            temp->NmPlayer = NmPlayer;
+            temp->NuPlayer = NuPlayer;
+            if (headDefender == NULL)
+            {
+                cur->child = temp;
+                temp->child = NULL;
+                headDefender = temp;
+                tailDefender = headDefender;
+            }
+            else
+            {
+                temp->child = NULL;
+                tailDefender->child = temp;
+                tailDefender = temp;
+            }
+        }
+        cur = cur->next;
+    }
+}
+
+// defender functions
+void tambahGoalkeeper(Node *temp, string NmPlayer, int NuPlayer)
+{
+    cur = parentHead;
+    while (cur != NULL)
+    {
+        if (cur->role == "goalkeeper")
+        {
+            temp = new Node;
+            temp->NmPlayer = NmPlayer;
+            temp->NuPlayer = NuPlayer;
+            cur->child = temp;
+            temp->child = NULL;
+            headGoalkeeper = temp;
+            tailGoalkeeper = tailGoalkeeper;
+        }
+        cur = cur->next;
+    }
+}
 
 // data terahkir
 void subtitusi_striker(Node *temp, string OutPlayer, string InPlayer, int NuPlayer)
 {
+    // cek apakah data memiliki nama anak yang dicari
     if (!checkChild(temp, OutPlayer, "striker"))
     {
         cout << "data tidak ditemukan" << endl;
     }
     else
     {
-        cur = parentHead;
-        while (cur != NULL)
+        // cek apakah data yang ingin di subtitusi data terahir
+        if (OutPlayer == tailStriker->NmPlayer)
         {
-            temp = cur;
-            if (cur->role == "striker")
+            cur = headStriker;
+            while (cur->child != tailStriker)
             {
-                while (temp != NULL)
-                {
-                    if (temp->child->NmPlayer == OutPlayer)
-                    {
-                        bef = temp;
-                    }
-                    if (temp->NmPlayer == OutPlayer)
-                    {
-                        del = temp;
-                        break;
-                    }
-                    temp = temp->child;
-                }
-                cout << "bef : " << bef->NmPlayer << endl;
-                cout << "del : " << del->NmPlayer << endl;
-                temp = new Node;
-                temp->NmPlayer = InPlayer;
-                temp->NuPlayer = NuPlayer;
-                bef->child = temp;
-                temp->child = del->child;
-                del->child = NULL;
-                delete del;
+                cur = cur->child;
             }
-            cur = cur->next;
+            temp = new Node;
+            temp->NmPlayer = InPlayer;
+            temp->NuPlayer = NuPlayer;
+            cur->child = temp;
+            temp->child = NULL;
+            tailStriker = temp;
+        }
+        // jika tidak lakukan ini
+        else
+        {
+            cur = parentHead;
+            while (cur != NULL)
+            {
+                temp = cur;
+                if (cur->role == "striker")
+                {
+                    while (temp != NULL)
+                    {
+                        if (temp->child->NmPlayer == OutPlayer)
+                        {
+                            bef = temp;
+                        }
+                        if (temp->NmPlayer == OutPlayer)
+                        {
+                            del = temp;
+                            break;
+                        }
+                        temp = temp->child;
+                    }
+                    temp = new Node;
+                    temp->NmPlayer = InPlayer;
+                    temp->NuPlayer = NuPlayer;
+                    bef->child = temp;
+                    temp->child = del->child;
+                    del->child = NULL;
+                    delete del;
+                }
+                cur = cur->next;
+            }
         }
     }
 }
@@ -160,17 +267,18 @@ void printParent(Node *temp)
 void printLocalChild(Node *temp, string role)
 {
     temp = parentHead;
+    cout << "\n";
     while (temp != NULL)
     {
         if (temp->role == role)
         {
-            cout << "parent: " << temp->role << endl;
+            cout << "Posisi: " << temp->role << endl;
             if (temp->child != NULL)
             {
                 cur = temp->child;
                 while (cur != NULL)
                 {
-                    cout << cur->NmPlayer << endl;
+                    cout << "(" << cur->NuPlayer << ") " << cur->NmPlayer << endl;
                     cur = cur->child;
                 }
             }
@@ -192,19 +300,26 @@ int main()
     createParent(temp, "defender");
     createParent(temp, "goalkeeper");
 
-    printParent(temp);
-    tambahStriker(temp, "a", 10);
-    tambahStriker(temp, "b", 20);
-    tambahStriker(temp, "c", 30);
-    tambahStriker(temp, "d", 40);
+    tambahStriker(temp, "Del Pino", 11);
+    tambahStriker(temp, "Marti", 9);
+
+    tambahMidfielder(temp, "Gassama", 8);
+    tambahMidfielder(temp, "Gaspar", 12);
+    tambahMidfielder(temp, "Camacho", 10);
+    tambahMidfielder(temp, "Ros", 6);
+
+    tambahDefender(temp, "Galves", 12);
+    tambahDefender(temp, "Alvarez", 2);
+    tambahDefender(temp, "Morillas", 3);
+    tambahDefender(temp, "Garcia", 5);
+
+    tambahGoalkeeper(temp, "Jiminez", 1);
 
     printLocalChild(temp, "striker");
-
-    subtitusi_striker(temp, "ca", "z", 100);
-
-    printLocalChild(temp, "striker");
-
-    cout << "kalo ini di cetak data no bug" << endl;
+    printLocalChild(temp, "midfielder");
+    printLocalChild(temp, "defender");
+    printLocalChild(temp, "goalkeeper");
+    cout << endl;
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-break;
+= NULL
